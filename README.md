1519d8d817973e23b1e98df80c6c995564e7a7a0
alejanCodeGF <alejan.gomez.fernandez@gmail.com>
2024-03-15 17:02:10 +0100

    First working version, with features (colors,clear)
    Now you can change the size, change color, and clear screen

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# odin-Etch-a-Sketch
+# Etch-a-Sketch
+
+![alt text](images/Screenshot_Etch_a_Sketch.png)
+
+This project is a web-based version of the Etch A Sketch game. It is created with HTML, CSS, and JavaScript. The game is fully functional, and it allows users to draw and create their own designs.
+
+## Features
+
+The game has a n x n grid of squares (n between 1 and 100, as you need)
+Users can draw lines by moving the cursor over the grid
+4 different colors
+Users can clear the screen by clicking the "Clear" button
+
+## Future updates
+
+1. Raimbow color, which randomize the squares’ RGB values with each interaction.
+2. Implement a progressive darkening effect where each interaction adds 10% more black or color to the square. The objective is to achieve a completely black square only after ten interactions.
+
+Live site: https://alejancodegf.github.io/odin-Etch-a-Sketch/
--- script.js
+++ script.js
@@ -1,18 +1,59 @@
-const SIZE = 10;
-// const SIZEX = 10; en un futuro poder hacer rectangulo el grid, no cuadrados como está ahora simetrico
-// const SIZEY = 10;
-const container = document.querySelector("#grid-container")
+let SIZE = 16;
+let current_color = "black";
 
-for (let i = 1; i <= SIZE; i++){ // creo 16 divs (en columna), con cada div 16 mas (en fila)
-    const my_BigDiv = document.createElement("div");
-    my_BigDiv.className = "bigGrid "+ i;
-    for (let j = 1; j <= SIZE; j++){
-        const my_div = document.createElement("div")
-        my_div.className = "grid "+ j;
-        my_div.addEventListener("mouseover", () => {
-            my_div.style["background-color"] = "blue";
-        })
-        my_BigDiv.appendChild(my_div);
+function generate_grid(size){
+    const container = document.querySelector("#grid-container");
+    for (let i = 1; i <= size; i++){ // n divs (in column), and in every div n more (in row) (by default n = 16)
+        const my_BigDiv = document.createElement("div");
+        my_BigDiv.className = "bigGrid";
+        for (let j = 1; j <= size; j++){
+            const my_div = document.createElement("div");
+            my_div.className = "grid";
+            my_div.addEventListener("mouseover", () => {
+                my_div.style["background-color"] = current_color;
+            })
+            my_BigDiv.appendChild(my_div);
+        }
+        container.appendChild(my_BigDiv);
     }
-    container.appendChild(my_BigDiv)
 }
+
+function remove_grid(){
+    const container = document.querySelector("#grid-container");
+    const my_BigDiv = document.querySelectorAll(".bigGrid");
+    my_BigDiv.forEach(div => {
+        container.removeChild(div);
+    });
+}
+
+const color_buttons = document.querySelectorAll(".color-button");
+color_buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        current_color = button.name;
+    }) 
+});
+
+const clear_button = document.querySelector(".clear");
+clear_button.addEventListener("click", () => {
+    remove_grid();
+    generate_grid(SIZE);
+});
+
+const size_button = document.querySelector(".size");
+size_button.addEventListener("click", () => {
+    temp = prompt("Size of the grid (n x n)");
+    if (temp == null) {}
+    else if (temp < 1 || temp > 100){
+        alert("Put a number between 1 and 100");
+    }
+    else if (temp != parseInt(temp)){
+        alert("Please, put a valid number");
+    }
+    else {
+        SIZE = temp
+        remove_grid();
+        generate_grid(SIZE);
+    }
+});
+
+generate_grid(SIZE); // start the game
